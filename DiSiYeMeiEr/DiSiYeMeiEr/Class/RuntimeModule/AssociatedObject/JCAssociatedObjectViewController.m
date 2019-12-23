6a294e567c327727b4d004acfd14478d87de671f
jiachen <624990742@qq.com>
2019-12-23 15:56:12 +0800

    使用系统的”lowercaseString“与”uppercaseString“两个方法做了runtime方法交换的例子

--- DiSiYeMeiEr/DiSiYeMeiEr/Class/RuntimeModule/AssociatedObject/JCAssociatedObjectViewController.m
+++ DiSiYeMeiEr/DiSiYeMeiEr/Class/RuntimeModule/AssociatedObject/JCAssociatedObjectViewController.m
@@ -11,55 +11,103 @@
 static void *kAlertyViewKey = @"kAlertyViewKey";
 @interface JCAssociatedObjectViewController ()
 @property(nonatomic,assign)NSInteger testIndex;
+@property(nonatomic,strong)NSArray *titleDataArr;
+@property(nonatomic,strong)NSMutableArray *dataScource;
+
+
 @end
 
 @implementation JCAssociatedObjectViewController
 
+- (NSArray *)titleDataArr{
+    if (!_titleDataArr) {
+        _titleDataArr = @[@"test1",@"test2",@"test3",@"test4",@"test5"];
+    }
+    return _titleDataArr;
+}
+
+- (NSMutableArray *)dataScource{
+    if (!_dataScource) {
+        _dataScource = [NSMutableArray array];
+    }
+    return _dataScource;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Runtime使用~关联对象";
-    // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
-    UIButton *btn1 = [UIButton  buttonWithType:UIButtonTypeCustom];
-    [btn1 setTitle:@"使用代理" forState:UIControlStateNormal];
-    btn1.backgroundColor = [UIColor orangeColor];
-    [btn1 addTarget:self action:@selector(testDemo1) forControlEvents:UIControlEventTouchUpInside];
-    
-    [self.view addSubview:btn1];
-    [btn1 makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(200);
-        make.centerX.equalTo(self.view.mas_centerX);
-        make.size.equalTo(CGSizeMake(160, 50));
-    }];
-    
-    
-    
-    for (NSInteger index = 0; index < 20; index++) {
+    [self setupUI];
+}
+
+
+- (void)setupUI{
+   
+    for (NSInteger index = 0; index < self.titleDataArr.count; index ++) {
         
+        UIButton *btn  = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.tag = (index + 1) *1000000;
+        [btn setTitle:self.titleDataArr[index] forState:UIControlStateNormal];
+        btn.backgroundColor = [UIColor RandomColor];//随机颜色
+        [btn addTarget:self action:@selector(btnClickEvent:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:btn];
+        [self.dataScource addObject:btn];
+        
+        
+        if (self.dataScource.count == 5) {//必须在最后统一布局
+            //FixedSpacing 水平左右间隔
+            //leadSpacing  第一个item距离手机屏幕的距离
+            //tailSpacing  最后一个item元素距离手机屏幕的距离
+            [self.dataScource mas_distributeViewsAlongAxis:MASAxisTypeHorizontal withFixedSpacing:10 leadSpacing:10 tailSpacing:10];
+            
+            // 设置array的垂直方向的约束
+            [self.dataScource mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.top.equalTo(100);
+                make.height.equalTo(100);
+            }];
+        }
     }
-    
-    
-    
-    
-    
-    
-    
-    UIButton *btn2 = [UIButton  buttonWithType:UIButtonTypeCustom];
-    [btn2 addTarget:self action:@selector(testDemo2) forControlEvents:UIControlEventTouchUpInside];
-    [btn2 setTitle:@"关联对象" forState:UIControlStateNormal];
-    btn2.backgroundColor = [UIColor redColor];
-    [self.view addSubview:btn2];
-    [btn2 makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(btn1.mas_bottom).offset(80);
-        make.centerX.equalTo(self.view.mas_centerX);
-        make.size.equalTo(CGSizeMake(160, 50));
-    }];
-    
+}
+
+
+
+- (void)btnClickEvent:(UIButton *)sender{
+    NSInteger tagValue = sender.tag;
+    switch (tagValue) {
+        case  1000000:
+            [self testDemo1];
+            break;
+            
+        case  2000000:
+            [self testDemo2];
+            break;
+            
+        case  3000000:
+            [self testDemo3];
+            break;
+            
+        case  4000000:
+            
+            break;
+            
+        case  5000000:
+            
+            break;
+            
+        default:
+            break;
+    }
     
 }
 
 
+
+
+
+
 #pragma mark - 使用代理实现弹窗
+//使用代理
 - (void)testDemo1{
     self.testIndex = 1;
     UIAlertView *alerty = [[UIAlertView  alloc] initWithTitle:@"使用代理的提示框" message:@"玩一玩" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
@@ -107,6 +155,69 @@ static void *kAlertyViewKey = @"kAlertyViewKey";
     [alerty show];
 }
 
+#pragma mark - 方法调配技术”黑盒方法“
+
+/**
+ * Exchanges the implementations of two methods.
+ *
+ * @param m1 Method to exchange with second method.
+ * @param m2 Method to exchange with first method.
+ *
+ * @note This is an atomic version of the following:
+ *  \code
+ *  IMP imp1 = method_getImplementation(m1);
+ *  IMP imp2 = method_getImplementation(m2);
+ *  method_setImplementation(m1, imp2);
+ *  method_setImplementation(m2, imp1);
+ *  \endcode
+ */
+//OBJC_EXPORT void
+//method_exchangeImplementations(Method _Nonnull m1, Method _Nonnull m2)
+//OBJC_AVAILABLE(10.5, 2.0, 9.0, 1.0, 2.0);
+
+- (void)testDemo3{
+    
+//    id (* IMP)(id,SEL,.....)
+  //方法交换前
+    
+    NSString *tesString = @"Thls ].S tHe StRiNg";
+    
+    NSLog(@"<============ 交换之前 ============>");
+    NSString *lowercaseString_before = [tesString lowercaseString];
+    NSLog(@"lowercaseString = %@",lowercaseString_before);
+    // Output: lowercaseString = THIS IS THE STRING
+    NSString *uppercaseString_before = [tesString uppercaseString];
+    NSLog(@"uppercaseString = %@",uppercaseString_before);
+    NSLog(@"====================");
+    
+    
+/*
+ 方法交换后
+   此函数的两个参数表示待交换的两个方法实现，
+   void method_exchangeimplementations(Method ml, Method m2J
+ 
+ 而方法实现则可通过下列函数获得：
+  Method class_getinstanceMethod(Class aClass, SEL aSelector)
+  */
+    
+    Method originalMethod = class_getInstanceMethod([NSString class], @selector(lowercaseString));
+    
+    Method swappedMethod = class_getInstanceMethod([NSString class], @selector(uppercaseString));
+    
+   //交换方法  
+    method_exchangeImplementations(originalMethod, swappedMethod);
+    
+    NSLog(@"<============ 交换之后 ============>");
+    
+    NSString *lowercaseString_after = [tesString lowercaseString];
+    NSLog(@"lowercaseString = %@",lowercaseString_after);
+          // Output: lowercaseString = THIS IS THE STRING
+    NSString *uppercaseString_after = [tesString uppercaseString];
+    NSLog(@"uppercaseString = %@",uppercaseString_after);
+    
+    
+    
+}
 
 
 
